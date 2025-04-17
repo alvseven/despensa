b291772e8e6f61accccf5bc2719145e36ec27f4b
Vicentesan <vikom.sanchez@gmail.com>
2025-04-17 20:51:37 -0300

    feat: implement date validation for product schemas

--- src/modules/products/update-product/schemas.ts
+++ src/modules/products/update-product/schemas.ts
@@ -1,12 +1,38 @@
+import { TZDate } from '@date-fns/tz';
+import { isAfter, isBefore } from 'date-fns';
 import { z } from 'zod';
 
 export const updateProductByIdRequestSchema = z.object({
   id: z.string().uuid(),
   userId: z.string().uuid(),
   name: z.string().min(1).optional(),
-  buyedAt: z.string(), // TODO - add date validation (it can be in the future) format: YYYY-MM-DD
-  category: z.string(),
-  expiresAt: z.string() // TODO - add date validation (it can be in the past) format: YYYY-MM-DD
-});
+  buyedAt: z.string().refine(
+    (date) => {
+      const currentDate = new TZDate(new Date(), 'America/Sao_Paulo');
+      const parsedDate = new Date(date);
+
+      const isAfterCurrentDate = isAfter(parsedDate, currentDate);
+
+      if (isAfterCurrentDate) return false;
+
+      return true;
+    },
+    {
+      message: 'Invalid date format. Please use YYYY-MM-DD format.'
+    }
+  ),  category: z.string(),
+  expiresAt: z.string().refine(
+    (date) => {
+      const currentDate = new TZDate(new Date(), 'America/Sao_Paulo');
+      const parsedDate = new Date(date);
+
+      if (isBefore(parsedDate, currentDate)) return false;
+
+      return true;
+    },
+    {
+      message: 'Invalid date format. Please use YYYY-MM-DD format.'
+    }
+  )});
 
 export type UpdateProductByIdInput = z.infer<typeof updateProductByIdRequestSchema>;
