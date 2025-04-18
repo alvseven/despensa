064263d2ea92fbb889f13e986df46d40c82041fe
Vicentesan <vikom.sanchez@gmail.com>
2025-04-17 21:01:06 -0300

    feat: add authenticate user with google route

---
+++ src/shared/libs/googleapis.ts
@@ -0,0 +1,10 @@
+import { envs } from '@/shared/config/env.ts';
+import { google } from 'googleapis';
+
+export const oauth2Client = new google.auth.OAuth2(
+  envs.GOOGLE_CLIENT_ID,
+  envs.GOOGLE_CLIENT_SECRET,
+  envs.GOOGLE_REDIRECT_URI,
+)
+
+export const oauth2 = google.oauth2('v2')
