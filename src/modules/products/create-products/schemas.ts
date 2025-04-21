9bef2f233b9135719489a9288f2c826b7262992f
Vicente Sanchez <Vikom.sanchez@gmail.com>
2025-04-21 18:23:46 -0300

    Date Validation Implementation for Product Schemas (#21)
    * feat: implement date validation for product schemas
    
    * chore: lint
    
    * fix: add PR review
    
    * chore: lint
    
    * feat: add proper error message
    
    * chore: lint
    
    ---------
    
    Co-authored-by: Alves <103443277+alvseven@users.noreply.github.com>

--- src/modules/products/create-products/schemas.ts
+++ src/modules/products/create-products/schemas.ts
@@ -1,12 +1,52 @@
-import { z } from 'zod';
+import { TZDate } from '@date-fns/tz';
+import { format, isAfter, isBefore, isMatch, parse } from 'date-fns';
+import { isValid, z } from 'zod';
+
+const dateFormat = 'yyyy-MM-dd';
 
 export const createProductRequestSchema = z.object({
   userId: z.string().uuid(),
   name: z.string().min(1),
-  buyedAt: z.string(), // TODO - add date validation (it cant be in the future) format: YYYY-MM-DD
-  notifications: z.array(z.string()).min(1).max(3), // TODO - add date validation (it cant be in the past) format: YYYY-MM-DD
+  buyedAt: z.string().superRefine((date, ctx) => {
+    if (!isMatch(date, dateFormat)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Invalid date format. Please use YYYY-MM-DD format.'
+      });
+    }
+
+    const currentDate = new TZDate(new Date(), 'America/Sao_Paulo');
+    const parsedDate = new Date(date);
+
+    const isAfterCurrentDate = isAfter(parsedDate, currentDate);
+
+    if (isAfterCurrentDate) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Date must be before current date.'
+      });
+    }
+  }),
+  notifications: z.array(z.string()).min(1).max(3),
   category: z.string(),
-  expiresAt: z.string() // TODO - add date validation (it cant be in the past) format: YYYY-MM-DD
+  expiresAt: z.string().superRefine((date, ctx) => {
+    if (!isMatch(date, dateFormat)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Invalid date format. Please use YYYY-MM-DD format.'
+      });
+    }
+
+    const currentDate = new TZDate(new Date(), 'America/Sao_Paulo');
+    const parsedDate = new Date(date);
+
+    if (isBefore(parsedDate, currentDate)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Date must be after current date.'
+      });
+    }
+  })
 });
 
 export type CreateProductInput = z.infer<typeof createProductRequestSchema>;
